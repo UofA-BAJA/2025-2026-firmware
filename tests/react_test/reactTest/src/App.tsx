95cbd7ffa2ccb455cddd7f6f3b94c5ec569af526
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2025-09-27 16:22:58 -0700

    added (baja logo + link to logo), rm (vite logo + link + <p>)

--- tests/react_test/reactTest/src/App.tsx
+++ tests/react_test/reactTest/src/App.tsx
@@ -23,22 +23,18 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <a
+          href="https://github.com/UofA-BAJA/2025-2026-firmware/tree/main"
+          target="_blank"
+        >
+          <img src="/baja_logo.jpg" className="logo" alt="Baja logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Line Graph Test</h1>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
