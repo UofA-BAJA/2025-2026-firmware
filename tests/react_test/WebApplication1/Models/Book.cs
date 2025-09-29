467fccb64f18bf991ffa0c190e8ab60ea93d16f4
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2025-09-29 02:02:05 -0700

    rm unneed .net files

---
+++ tests/react_test/WebApplication1/Models/Book.cs
@@ -0,0 +1,12 @@
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public String Title { get; set; } = null!;
+        public String Author { get; set; } = null!;
+
+         public String YearPublished { get; set; } = null!;
+
+    }
+}
