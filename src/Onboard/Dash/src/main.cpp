7275d6f50d46c8de37f3765ad729abb555b65e7d
MoonriseSunset <69921861+MoonriseSunset@users.noreply.github.com>
2026-04-04 11:57:41 -0700

    Set up Firmware and cleaned up after merge

---
+++ src/Onboard/Dash/src/main.cpp
@@ -0,0 +1,24 @@
+/*
+* DASH 2025-2026 Firmware
+* Version: 1.0.0
+* 
+*/
+
+// Includes -----------------
+
+// Core Libraries
+#include <Arduino.h>
+#include "CANTProtocol.h"
+
+// Specific Libraries
+
+// Variables ----------------
+
+// Instantiations -----------
+
+CANTProtocol CAN();
+
+// Functions ----------------
+
+// MAIN ---------------------
+
