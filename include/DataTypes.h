9ff19dbfd5a87456ba17c817362bb749e38a8ddf
karsteny <42128680+karsteny@users.noreply.github.com>
2026-04-23 20:24:31 -0700

    add struct and datatype

--- include/DataTypes.h
+++ include/DataTypes.h
@@ -8,7 +8,7 @@
 // It is also the code that will serve as the "smift amount" for the mask value,
 // as described in the live data packet protocol 
 enum DataType{
-    KEEP_ALIVE = 0x00,
+    //oops what happened to zero???
     WHEEL_RPM = 0x01,
     CAR_SPEED = 0x02,
     MOTOR_RPM = 0x03,
@@ -18,7 +18,10 @@ enum DataType{
     SHOCK_DISPLACEMENT = 0x07,
     CVT_TEMPERATURE = 0x08,
     DISTANCE = 0x09,
-    ERRORS = 0x0A,
+    GPS_POSITION = 0x10,
+
+    //...
+    ERRORS = 0xAA,
     // Datatypes at or below 255 (0xFF) are the only ones that can be sent via radio, others will be ignored
 };
 
@@ -51,5 +54,11 @@ typedef struct BrakePressure{
     float front;
     float rear;
 }BrakePressure;
+
+typedef struct GPSPosition{
+    float longitude;
+    float latitude;
+    float atltitude;
+}GPSPosition;
 
 #endif
