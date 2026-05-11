78e1a62fd91f4f28cc7007692ac365d28051e51b
karsteny <42128680+karsteny@users.noreply.github.com>
2026-05-10 19:01:02 -0700

    work on pit radio esp32

--- src/Onboard/PitRadio/src/main.cpp
+++ src/Onboard/PitRadio/src/main.cpp
@@ -12,6 +12,14 @@ const uint32_t USB_DELIMITER = 0xAAAAAAAA;
 uint8_t outputBuf[RH_RF95_MAX_MESSAGE_LEN + 10];
 uint16_t outputBufLength = 0;
 
+uint8_t commandQueue[256];
+uint8_t commandQueueLength = 0;
+int newFrequency = -1;
+uint8_t newDataTypeMask[32];
+
+uint8_t radioOutputBuf[RH_RF95_MAX_MESSAGE_LEN];
+uint16_t radioOuputBufLength = 0;
+
 // Singleton instance of the radio driver
 RH_RF95 rf95(RADIO_CS_PIN, RADIO_INT_PIN);
 
@@ -27,15 +35,18 @@ void setup()
     if (!rf95.init())
     {
         // Serial.println("RF95 init failed. Try restarting power.");
-        while (1)
-            ;
+        pinMode(2, OUTPUT);
+        while (1){
+            digitalWrite(2, HIGH);
+            delay(400);
+            digitalWrite(2, LOW);
+            delay(400);   
+        }
     }
 
     rf95.setTxPower(23, false); // Sets maximum power for the LoRa module
-    rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128);
-
-    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-    rf95.setFrequency(915.0);
+    rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128); //Sets maximum bandwidth
+    rf95.setFrequency(915.0); //Sets 915 MHz so it's legal
 
     //Setup LoRa IDs, if we want to (probably should)
     // rf95.setThisAddress(1);
@@ -43,80 +54,28 @@ void setup()
     // rf95.setHeaderFrom(1);
     rf95.setHeaderFlags(0x00); // clear flags
 
-    // Serial.println("Init OK");
 }
 
 //put these not on the stack
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 uint8_t len = sizeof(buf);
 
+uint8_t serialLastByte;
+uint8_t intQueue[4];
+
 void loop()
 {
-    if (rf95.waitAvailableTimeout(2000))
+    if (rf95.waitAvailableTimeout(500))
     {
+        digitalWrite(2, LOW);
         // Should be a message for us now
         len = sizeof(buf);
         if (rf95.recv(buf, &len))
         {
             int ind = 0;
-            
-            // Serial.println((int)len, BIN);
-            // if(len > 0){
-            //     Serial.println("real data recieved!!!!");
-            // }else{
-            //     Serial.println("keep alive");
-            // }
-            //Temporary parsing, eventually we're sending this raw via serial
-            // while(ind < len){
-
-            //     //Make sure there's literally enough 
-            //     if(len - ind < (2 + sizeof(float))){
-            //         break;
-            //     }
-            
-            //     unsigned char id;
-            //     memcpy(&id, buf + ind, 1);
-            //     ind++;
-
-            //     float timestamp;
-            //     memcpy(&timestamp, buf+ind, sizeof(float));
-            //     ind += sizeof(float);
-
-            //     unsigned char dataLength;
-            //     memcpy(&dataLength, buf+ind, 1);
-            //     ind++;
-
-            //     Serial.print("ID: ");
-            //     Serial.println(id, HEX);
-            //     Serial.print("Data Length: ");
-            //     Serial.println(dataLength);
-            //     Serial.print("Timestamp: ");
-            //     Serial.println(timestamp);
-            //     Serial.println("data 1 byte at a time below: ");
-
-
-            //     for (int i = ind; i < ind + dataLength; i++)
-            //     {
-            //         Serial.print(buf[i], HEX);
-            //         Serial.print(",");
-                    
-            //     }
-            //     ind += dataLength;
-            //     Serial.println();
-               
-            // }
-
-            
-          
+                  
             uint8_t headerFlags = rf95.headerFlags();
             int16_t lastRSSI = rf95.lastRssi();
-
-            // if(headerFlags & (1 << 2)){
-            //     Serial.println("Overload detected!");
-            // }
-
-            // Serial.print("rssi: ");
-            // Serial.println(lastRSSI);
 
             //flags (1 byte) + rssi (2 bytes) + message (n bytes) + delimiters (4 bytes)
             outputBufLength = len + 7;
@@ -128,30 +87,88 @@ void loop()
             
             Serial.write(outputBuf, outputBufLength);
             
-            //TODO: print it lol
 
             if(headerFlags & 1){
-                // delay(10);
-                uint8_t returnData[2];
-                // returnData[0] = 101;
-                // returnData[1] = 100;
-                rf95.send(returnData, 0);
-                rf95.waitPacketSent();
-                // Serial.println("Send command response packet");
+                //build up the message
+                int i = 0;
+                bool stuffRemains = false;
+                for(; i < commandQueueLength; i++){
+                    if(commandQueue[i] == 0){
+                        if(RH_RF95_MAX_MESSAGE_LEN - radioOuputBufLength > 4){
+                            memcpy(radioOutputBuf + radioOuputBufLength, &newFrequency, 4);
+                            radioOuputBufLength += 4;
+                        }else{
+                            stuffRemains = true;
+                        }
+                    }else if(commandQueue[i] == 1){
+                        if(RH_RF95_MAX_MESSAGE_LEN - radioOuputBufLength > 32){
+                            memcpy(radioOutputBuf + radioOuputBufLength, newDataTypeMask, 32);
+                            radioOuputBufLength += 32;
+                        }else{
+                            stuffRemains = true;
+                        }
+                    }else if(radioOuputBufLength == RH_RF95_MAX_MESSAGE_LEN){
+                        //shift everything down for the next cycle
+                        memcpy(commandQueue, commandQueue + i, commandQueueLength - i);
+                        commandQueueLength = commandQueueLength - i;
+                        break;
+                    }else{
+                        radioOutputBuf[radioOuputBufLength] = commandQueue[i];
+                        radioOuputBufLength++;
+                    }
+                }
+                
+                rf95.send(radioOutputBuf, radioOuputBufLength);
+
+                if(stuffRemains){
+                    memcpy(commandQueue, commandQueue + i, commandQueueLength - i);
+                    commandQueueLength = commandQueueLength - i;
+                }else{
+                    commandQueueLength = 0;
+                }
             }
         }
         else
         {
-            // Serial.println("RX Failure");
+            // RX Failure (but not "nothing recieved")
         }
-        // Serial.print("Last SNR: ");
-        // Serial.println(rf95.lastSNR());
-        // Serial.println();
-        // Serial.println();
     }
     else
     {
-        // Serial.println("No Connection");
+        digitalWrite(2, HIGH);
+        // Nothing Recieved
+    }
+
+    while(Serial.available() > 0){
+        serialLastByte = (uint8_t)Serial.read();
+
+        if(commandQueueLength < 256){
+            //Change frequency
+            if(serialLastByte == 0){
+                int read = -1;
+                for(int i = 0; i < 4; i++){
+                    read = Serial.read();
+                    if(read == -1) break; //if we read nothing, stop
+                    intQueue[i] = (uint8_t)read;
+                }
+                memcpy(&newFrequency, intQueue, 4);
+            }
+            //Change Datatype mask
+            else if(serialLastByte == 1){
+                int read = -1;
+                for(int i = 0; i < 32; i++){
+                    read = Serial.read();
+                    if(read == -1) break; //if we read nothing, stop
+                    newDataTypeMask[i] = (uint8_t)read;
+                }
+            }
+
+            //only add to queue at the end since we could be leaving malformed data otherwise
+            commandQueue[commandQueueLength] = serialLastByte;
+            commandQueueLength++;
+        }
+       
+
     }
     
 }
