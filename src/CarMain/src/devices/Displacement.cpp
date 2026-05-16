26ea80d35a5fcb102ed8002e694b1e1de535c1c7
BAJA ELECTRICAL <uabaja@gmail.com>
2026-05-16 14:21:08 -0700

    wip

---
+++ src/CarMain/src/devices/Displacement.cpp
@@ -0,0 +1,11 @@
+#include "Displacement.h"
+
+namespace BajaWildcatRacing
+{
+
+    Displacement::Displacement(CANDispatcher& canDispatcher, byte deviceId) : CANDevice(canDispatcher, deviceId) {
+
+    }
+
+    
+}
---
+++ src/CarMain/src/devices/include/Displacement.h
@@ -0,0 +1,24 @@
+#ifndef DISPLACEMENT_H
+#define DISPLACEMENT_H
+
+#include "CANDevice.h"
+
+namespace BajaWildcatRacing
+{
+
+    class Displacement : CANDevice {
+
+        public:
+
+            Displacement(CANDispatcher& candispatcher, byte deviceId);
+
+            typedef struct ShockDisplacementPair
+
+        private:
+            
+
+    };
+
+}
+
+#endif
